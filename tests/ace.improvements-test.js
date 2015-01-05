5fcad062d28da7263459abf938afe3c0c99e33fe
Robert Krahn <robert.krahn@gmail.com>
2015-01-05 02:03:09 -0800

    editor.setSelection, accepting multiple forms

--- tests/ace.improvements-test.js
+++ tests/ace.improvements-test.js
@@ -1,19 +1,58 @@
-/*global process, beforeEach, afterEach, describe, it*/
-
-var env = typeof module !== "undefined" && module.require ? module.require("../env") : lively['lively.lang_env'];
-var escodegen = env.escodegen, lang = env['lively.lang'], expect, ast;
-if (env.isCommonJS) {
-  var chai = module.require('chai');
-  chai.use(require('chai-subset'));
-  expect = chai.expect;
-  ast = require('../index');
-} else { expect = chai.expect; ast = env['lively.ast']; }
+/*global process, expect, beforeEach, afterEach, describe, it*/
 
 describe('improvements', function() {
-  it('saveExcursion', function() {
 
-    expect(ast.parse("1 + 2")).deep.property("body[0].type")
-      .equals("ExpressionStatement");
+  var ed;
+  beforeEach(function() {
+    ed = createEditor("this is a\ntest\n\ntext\n");
   });
+
+  afterEach(function() {
+    if (ed) {
+      ed.destroy();
+      ed.container.parentNode.removeChild(ed.container);
+    }
+  });
+
+  describe("selection", function() {
+
+    it("selects via range string", function() {
+      ed.setSelection("[0/2]->[0/4]");
+      expect(ed).to.have.selection("[0/2]->[0/4]");
+      expect(ed.getSelectedText()).eq("is");
+      expect(ed.selection.isBackwards()).eq(false);
+    });
+
+    it("selects via range string backward", function() {
+      ed.setSelection("[0/4]->[0/2]");
+      expect(ed.getSelectedText()).eq("is");
+      expect(ed.selection.isBackwards()).eq(true);
+    });
+
+    it("selects via index", function() {
+      ed.setSelection(1,3);
+      expect(ed.getSelectedText()).eq("hi");
+      expect(ed.selection.isBackwards()).eq(false);
+    });
+
+    it("selects via index backwards", function() {
+      ed.setSelection(3,1);
+      expect(ed.getSelectedText()).eq("hi");
+      expect(ed.selection.isBackwards()).eq(true);
+    });
+
+    it('saveExcursion', function(done) {
+      expect(ed).to.exist();
+      ed.setSelection("[0/2]->[0/4]");
+      ed.saveExcursion(function(reset) {
+        ed.selection.clearSelection();
+        expect(ed).to.have.selection("[0/4]->[0/4]");
+        reset();
+        expect(ed).to.have.selection("[0/2]->[0/4]");
+        done()
+      });
+    });
+  })
+
 
 });
